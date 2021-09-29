0074d22c97d4c2077113337a814c4515a4973781
James Brownie <jamespbrownie@gmail.com>
2021-09-28 21:17:35 -0500

    commit tree 3d9f91f6c6994fc2a536509f731f6e70411829f0

--- index.js
+++ index.js
@@ -1,1 +1,124 @@
 // Code your solution here
+
+
+
+const findMatching = (someArray, driverName) => {
+    let newArray = someArray.filter( aName => aName.toLowerCase() == driverName.toLowerCase() )
+    return newArray
+}
+
+
+
+
+const fuzzyMatch = (someArray, driverName) => {
+    let newArray = someArray.filter( aName => aName.toLowerCase().slice(0,2) == driverName.toLowerCase().slice(0,2) )
+    return newArray
+}
+
+
+
+const matchName = (someArray, someString) => {
+    let newArray = someArray.filter( anObject => anObject.name == someString)
+    return newArray
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function findMatching (array, name) {
+//     let matchingName = array.filter( (driver) => {
+//             return driver.toLowerCase() === name.toLowerCase()
+//     })
+//     return matchingName 
+// }
+
+
+
+// function fuzzyMatch (array, partialName) {
+//     let matchingName = array.filter( (driver) => {
+//         return driver.substring(0, partialName.length) == partialName
+//     })
+//     return matchingName 
+// }
+
+
+// function matchName (array, string) {
+//     let matchingName = array.filter( (driverName) => {
+//         return driverName.name === string
+//     })
+//     return matchingName
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function findMatching (array, name) {
+    let matchingDrivers = array.filter( function (driver) {
+        return driver.toLowerCase() === name.toLowerCase()
+    })
+    return matchingDrivers
+}
+
+
+function fuzzyMatch(array, string) {
+    let match = array.filter( function(driverName) {
+        return driverName.substring(0, string.length ) == string
+    })
+    return match 
+}
+
+
+function matchName(array, string) {
+    let nameMatch = array.filter( function (driverName) { 
+        return driverName.name === string
+    })
+    return nameMatch
+}
+
+// function findMatching (array, name) {
+//     let matchingDrivers = array.filter( function (driver) {
+//         return driver.toLowerCase() === name.toLowerCase()
+//     })
+//     return matchingDrivers
+// }
+
+*/
